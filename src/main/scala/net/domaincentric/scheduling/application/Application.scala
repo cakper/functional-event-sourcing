a8f5f78cb2010bf771568984e3888f8d4a45124c
Kacper Gunia <kacper@gunia.me>
2020-06-11 17:17:45 +0100

    Move classes

--- src/main/scala/net/domaincentric/scheduling/application/Application.scala
+++ src/main/scala/net/domaincentric/scheduling/application/Application.scala
@@ -1,7 +1,7 @@
-package net.domaincentric.scheduling
+package net.domaincentric.scheduling.application
 
 import cats.effect.ExitCode
-import monix.eval.{Task, TaskApp}
+import monix.eval.{ Task, TaskApp }
 
 object Application extends TaskApp {
   override def run(args: List[String]): Task[ExitCode] = ???
