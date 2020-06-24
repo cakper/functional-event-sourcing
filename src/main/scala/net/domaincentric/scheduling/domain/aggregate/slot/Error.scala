59235f65945186200050b53c1a78d963a0e0cc44
Kacper Gunia <kacper@gunia.me>
2020-06-24 16:09:12 +0100

    Remove slot aggregate

--- src/main/scala/net/domaincentric/scheduling/domain/aggregate/slot/Error.scala
+++
@@ -1,9 +0,0 @@
-package net.domaincentric.scheduling.domain.aggregate.slot
-
-sealed trait Error
-
-case class SlotAlreadyScheduled() extends Error
-case class SlotNotScheduled()     extends Error
-case class SlotAlreadyBooked()    extends Error
-case class SlotNotBooked()        extends Error
-case class SlotAlreadyStarted()   extends Error
